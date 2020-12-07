13760e783955b5035fed2008d9d700e7157457b7
Lucas-Camilo <htslucasv@gmail.com>
2020-12-07 20:21:00 -0300

    Comecando tela para capturar video a ser analisado

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang.builder import Builder
 
-
 # from kivy.core.window import Window
 # import Cores
 # Window.clearcolor = Cores.AZUL
@@ -17,17 +16,41 @@ class Gerenciador(ScreenManager):
 
 
 class Login(Screen):
-    class Entrar(Screen):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def coletaDados(self):
+        user = self.ids.user.text
+        senha = self.ids.password.text
+        return [user, senha]
+
+    def login(self):
+        usersenha: list = self.coletaDados()
+        is_valido = self.valida(usersenha)
+        if is_valido:
+            self.manager.current = "main"
+        else:
+            print("Dados incorretos")
+
+    def valida(self, dados:list):
+        isvalido: bool = False
+        if dados[0] != "" and dados[1] != "":
+            isvalido = True
+        return isvalido
 
 
 class Main(Screen):
-    class Entrar(Screen):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
     pass
+
+
+class GetVideo(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def get_video(self):
+        print("pegando Video... ")
 
 
 class Inicial(App):
